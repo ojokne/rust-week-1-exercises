e2469843289e4ce33996fa8833313d72fe16246a
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2025-06-20 05:57:41 +0300

    Implement extract_tx_version function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
 // Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+    let raw_bytes = match hex::decode(raw_tx_hex) {
+        Ok(bytes) => bytes,
+        Err(_) => return Err("Hex decode error".to_string()),
+    };
+
+    if raw_bytes.len() < 4 {
+        return Err("Transaction data too short".to_string());
+    }
+
+    let version_bytes: [u8; 4] = raw_bytes[0..4].try_into().unwrap();
+
+    let version = u32::from_le_bytes(version_bytes);
+
+    Ok(version)
 }
